ef0ef5a475ee765db89fa910e6facc7d4614b6f1
Seth Helfgott <sethleland@gmail.com>
2013-09-15 22:51:53 -0400

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ RailsApp::Application.routes.draw do
   root to: 'static_pages#home'
 
   match '/signup', to: 'users#new'
-
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/help', to: 'static_pages#help'
 
   match '/about', to: 'static_pages#about'
@@ -11,6 +12,10 @@ RailsApp::Application.routes.draw do
   match '/contact', to: 'static_pages#contact'
 
   resources :users
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+
 
 
 
